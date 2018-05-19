3f986178680e47c2242ab1aceb8b23eb1b10d164
zhuyst <13553371651@139.com>
2018-05-19 16:55:31 +0800

    Server注释

--- ChatRoom.Server/Program.cs
+++ ChatRoom.Server/Program.cs
@@ -10,8 +10,14 @@ namespace ChatRoom.Server
 {
     class Program
     {
+        /// <summary>
+        /// 在线用户
+        /// </summary>
         private static readonly Dictionary<int,User> OnlineUsers = new Dictionary<int, User>();
 
+        /// <summary>
+        /// 当前最新的用户ID
+        /// </summary>
         private static int _idNow;
 
         static void Main(string[] args)
@@ -21,6 +27,8 @@ namespace ChatRoom.Server
             ChatRoomRemote.LoginEvent += user =>
             {
                 Console.WriteLine($"新用户\t{user.Name}\t加入到了聊天室");
+
+                // 进行自增ID生成
                 user.Id = ++_idNow;
                 OnlineUsers[user.Id] = user;
                 return user;
@@ -36,27 +44,32 @@ namespace ChatRoom.Server
             };
             ChatRoomRemote.GetUsersEvent += () => OnlineUsers;
 
+            // 配置信息
             var tcpProperties = new Hashtable
             {
                 ["name"] = "ChatRoom",
                 ["port"] = 8080
             };
 
+            // 设置序列化等级
             var tcpClientSinkProvider = new BinaryClientFormatterSinkProvider();
             var tcpServerSinkProvider = new BinaryServerFormatterSinkProvider
             {
                 TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full
             };
 
+            // 注册信道
             var channel = new TcpChannel(tcpProperties,tcpClientSinkProvider,tcpServerSinkProvider);
             ChannelServices.RegisterChannel(channel,false);
 
+            // 注册远程对象
             RemotingConfiguration.RegisterWellKnownServiceType(typeof(ChatRoomRemote),
                 "ChatRoom", WellKnownObjectMode.Singleton);
 
             Console.WriteLine("服务器已启动，输入任意键关闭");
             Console.ReadLine();
 
+            // 注销信道
             ChannelServices.UnregisterChannel(channel);
         }
     }
